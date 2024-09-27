970dee5eba79789473a13d45d1af729298bd7e88
kkjjkkll1111 <kkjjkkll1111@gmail.com>
2024-09-27 15:23:21 +0900

    v0.3 type

--- week05/main.go
+++ week05/main.go
@@ -3,19 +3,23 @@ package main
 import (
 	"fmt"
 	"math"
+	"reflect"
 	"strings"
 )
 
 func main(){
 	//var i int = 55
-	var f float32 = 3.99
+	//var f float32 = 3.99
 	//var i int 
 	//i = 55
 
 	i := 55
+	//i := "55"
+	f := 3.99
+	fmt.Println(reflect.TypeOf(i),reflect.TypeOf(f))
 	fmt.Println(f, math.Ceil(3.49))
 	fmt.Println(strings.Title("kim inha"))
-	fmt.Printf("%d\n", i)
+	fmt.Printf("%dcd \n", i)
 	fmt.Print("i는", i, "\n")
 	fmt.Println("i는", i)
 
