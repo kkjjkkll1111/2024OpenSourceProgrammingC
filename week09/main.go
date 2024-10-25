403da30ee01b7e6d78419ba62b8f732fe27c57f3
kkjjkkll1111 <kkjjkkll1111@gmail.com>
2024-10-25 14:24:45 +0900

    v1.6 Guess game

--- week09/main.go
+++ week09/main.go
@@ -1,13 +1,42 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"time"
+	"log"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
 	rand.Seed(time.Now().Unix())
-	target := rand.Intn(6)
-	fmt.Println(target)
+	answer := rand.Intn(6) + 1
+	fmt.Println(answer)
+
+	fmt.Print("숫자 입력 : ")
+	in := bufio.NewReader(os.Stdin)
+	i, err := in.ReadString('\n')
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	i = strings.TrimSpace(i) // 줄바꿈등 제거. 파이썬 strip 함수와 비슷
+	guess, err := strconv.Atoi(i)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(guess)
+
+	if answer == guess{
+		fmt.Println("정답입니다")
+	} else if answer > guess{
+		fmt.Println("입력하신 수는 정답보다 작은 수 입니다. LOW")
+	} else{
+		fmt.Println("입력하신 수는 정답보다 큰 수 입니다. HIGH")
+	}
+	
 }
