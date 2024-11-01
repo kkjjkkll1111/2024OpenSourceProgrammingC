37c685fd5836a8a012e4e91a53385cccc25d9513
kkjjkkll1111 <kkjjkkll1111@gmail.com>
2024-11-01 15:37:06 +0900

    v3.0 Prime number

--- week10/main.go
+++ week10/main.go
@@ -9,6 +9,32 @@ import (
 	"strings"
 )
 
+func isPrime(n int) bool {
+	//var isPrime bool = true
+
+	if n <= 1 {
+		//isPrime = false
+		return false
+	} else if n == 2 {
+		//isPrime = true
+		return true
+	} else if n%2 == 0 {
+		//isPrime = false
+		return false
+	} else {
+		j := 3
+		for j*j <= n {
+			if n%j == 0 {
+				//isPrime = false
+				return false
+			}
+			fmt.Printf("%d ", j)
+			j = j + 2
+		}
+	}
+	return true
+}
+
 func main() {
 	fmt.Print("input your number : ")
 	in := bufio.NewReader(os.Stdin)
@@ -23,28 +49,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var isPrime bool = true
-
-	if n <= 1 {
-		isPrime = false
-	} else if n == 2 {
-		isPrime = true
-	} else if n%2 == 0 {
-		isPrime = false
-	} else {
-		j := 3
-		//for j <= int(math.Sqrt(float64(n))) {
-		for j*j <= n {
-			if n%j == 0 {
-				isPrime = false
-				break
-			}
-			fmt.Printf("%d ", j)
-			j = j + 2
-		}
-	}
-
-	if isPrime {
+	if isPrime(n) {
 		fmt.Printf("%d is prime number", n)
 	} else {
 		fmt.Printf("%d is NOT prime number", n)
