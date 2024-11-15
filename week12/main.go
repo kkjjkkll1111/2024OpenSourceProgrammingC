d48ecd9c04ddab9a4b63c17461e9f5b828fbd529
kkjjkkll1111 <kkjjkkll1111@gmail.com>
2024-11-15 15:23:55 +0900

    v3.9 Array

--- week12/main.go
+++ week12/main.go
@@ -2,46 +2,18 @@ package main
 
 import (
 	"fmt"
-	"time"
+
+	"github.com/headfirstgo/keyboard"
 )
 
 func main() {
-	var scores [3]int
-	scores[1] = 90
-	fmt.Println(scores[1], scores[0]) //,scores[3])
-	fmt.Printf("%#v\n", scores)
-	fmt.Println(scores)
-
-	// for i := 0; i <= len(scores); i++ { //0 90 0 panic: runtime error: index out of range [3] with length 3
-	for i := 0; i<len(scores); i++{
-		fmt.Printf("%d ", scores[i])
+	var gpas [3]float64
+	for i := 0; i < len(gpas); i++ {
+		fmt.Print("Input float number : ")
+		gpas[i], _ = keyboard.GetFloat()
 	}
 
-	// var dates [3]time.Time
-	// dates[1] = time.Unix(1947200001, 0)
-	// fmt.Println(dates[1])
-
-	// dates := [3]time.Time{time.Unix(0, 0), time.Unix(1, 0), time.Unix(1947200001, 0)}
-	// fmt.Println(dates[0], dates[1], dates[2])
-
-	// dates := [3]time.Time{
-	// 	time.Unix(0, 0),
-	// 	time.Unix(1, 0),
-	// 	time.Unix(1947200001, 0), //need comma
-	// }
-	// fmt.Println(dates[0], dates[1], dates[2])
-	fmt.Println()
-	dates := [3]time.Time{
-		time.Unix(0, 0),
-		time.Unix(1, 0),
-		time.Unix(1947200001, 0)}
-	// fmt.Println(dates[0], dates[1], dates[2])
-	// fmt.Printf("%#v\n", dates)
-	// fmt.Println(dates)
-	// for i, date := range dates{
-	// 	fmt.Println(i, date)
-	// }
-	for _, date := range dates{   //like python style, SAFE!
-		fmt.Println(date)
+	for _, gpa := range gpas {
+		fmt.Println(gpa)
 	}
 }
